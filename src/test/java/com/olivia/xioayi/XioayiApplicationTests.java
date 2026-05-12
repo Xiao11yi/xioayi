7e90423da714d805c7709a4cba072340f8811b4f
oi <486978649@qq.com>
2026-05-12 10:25:35 +0800

    init

---
+++ src/test/java/com/olivia/xioayi/XioayiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivia.xioayi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XioayiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
